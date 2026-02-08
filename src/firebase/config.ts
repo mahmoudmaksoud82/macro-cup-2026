322b5b52f870a996f7a2a1a5195517da9e3c3a81
mahmoud maksoud <mahmoudmaksoud@gmail.com>
2026-02-08 11:10:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3088747826-e2e6c",
+  "appId": "1:277144831412:web:526205e3052453b4c3dee1",
+  "apiKey": "AIzaSyCJRnNDQF5lnVYiJw2-KuJQUyE9Tujnzfo",
+  "authDomain": "studio-3088747826-e2e6c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "277144831412"
+};
