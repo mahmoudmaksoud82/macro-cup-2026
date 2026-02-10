f8557bb712c5bdb54a2f95572420165cb2c39662
mahmoud maksoud <mahmoudmaksoud@gmail.com>
2026-02-10 07:46:41 +0000

    صغر الصوره بنسبة 40%

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -136,12 +136,12 @@ export default function RegistrationForm() {
 
   return (
     <Card className="w-full max-w-2xl mx-auto shadow-xl border-t-4 border-t-primary bg-card/95 relative overflow-hidden">
-      <div className="absolute top-[-10px] left-[-30px] w-[166px] h-[166px] pointer-events-none z-0">
+      <div className="absolute top-[-10px] left-[-30px] w-[100px] h-[100px] pointer-events-none z-0">
         <Image 
           src="/logo.png"
           alt="logo"
-          width={166}
-          height={166}
+          width={100}
+          height={100}
           className="object-contain"
           priority
           quality={100}
