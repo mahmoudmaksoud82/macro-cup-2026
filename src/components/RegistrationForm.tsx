1fdfa484a039f81ae34751ecac5ee40729fdecba
mahmoud maksoud <mahmoudmaksoud@gmail.com>
2026-02-10 07:53:37 +0000

    هات الصورة يمن قليلا

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -136,7 +136,7 @@ export default function RegistrationForm() {
 
   return (
     <Card className="w-full max-w-2xl mx-auto shadow-xl border-t-4 border-t-primary bg-card/95 relative overflow-hidden">
-      <div className="absolute top-[10px] left-[10px] w-[72px] h-[72px] pointer-events-none z-0">
+      <div className="absolute top-[10px] left-[20px] w-[72px] h-[72px] pointer-events-none z-0">
         <Image 
           src="/logo.png"
           alt="logo"
