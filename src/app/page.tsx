40e1d36c151644d4aeffd59939ec524abeeec238
mahmoud maksoud <mahmoudmaksoud@gmail.com>
2026-02-10 10:17:16 +0000

    بدل الجملة Macro Cup Ramadan 2026 (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       {/* Hero Content */}
       <div className="container mx-auto px-4 mb-12 text-center">
         <h1 className="text-5xl md:text-7xl font-bold font-headline mb-4 text-primary drop-shadow-sm">
-          Macro CUP Ramadan 2026
+          Macro Cup Ramadan 2026
         </h1>
         
         <div className="space-y-6">
@@ -125,4 +125,3 @@ export default function Home() {
     </main>
   );
 }
-
